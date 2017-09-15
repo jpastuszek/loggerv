25fbc2d7467947f46de88ae4b05fbc4246bccff7
clux <sszynrae@gmail.com>
2017-09-15 22:28:35 +0100

    Update documentation

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["libc"] = [];
 implementors["log"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"log/struct.SetLoggerError.html\" title=\"struct log::SetLoggerError\">SetLoggerError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"log/struct.ShutdownLoggerError.html\" title=\"struct log::ShutdownLoggerError\">ShutdownLoggerError</a>",];
 
             if (window.register_implementors) {
