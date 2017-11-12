ca0d5db7a11f69c675bf98bac0f1166232d595b9
Christopher Field <cfield2@gmail.com>
2017-11-12 11:16:46 -0500

    Add config example to manifest

--- examples/config.rs
+++ examples/config.rs
@@ -1,3 +1,9 @@
+//! An example using the Builder pattern API to configure the logger based on command line
+//! arguments.
+//!
+//! The [clap](https://crates.io/crates/clap) argument parser is used in this example, but loggerv
+//! works with any argument parser.
+
 #[macro_use] extern crate log;
 extern crate loggerv;
 extern crate clap;
@@ -6,19 +12,19 @@ use clap::{Arg, App};
 
 fn main() {
     let args = App::new("app")
-                   .arg(Arg::with_name("v")
-                        .short("v")
-                        .multiple(true)
-                        .help("Sets the level of verbosity"))
-                   .arg(Arg::with_name("debug")
-                        .short("d")
-                        .long("debug")
-                        .help("Adds module path and line numbers to log statements"))
-                   .arg(Arg::with_name("no-color")
-                        .short("n")
-                        .long("no-color")
-                        .help("Disables colorized output"))
-                   .get_matches();
+       .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+       .arg(Arg::with_name("debug")
+            .short("d")
+            .long("debug")
+            .help("Adds the module path and line numbers to log statements"))
+       .arg(Arg::with_name("no-color")
+            .short("n")
+            .long("no-color")
+            .help("Disables colorized output"))
+       .get_matches();
 
     loggerv::Logger::new()
         .verbosity(args.occurrences_of("v"))
@@ -29,8 +35,9 @@ fn main() {
         .unwrap();
 
     error!("This is always printed to stderr");
-    warn!("This too, and it's printed to stderr");
-    info!("This is optional info");
-    debug!("This is optional debug");
-    trace!("This is optional trace");
+    warn!("This too is always printed to stderr");
+    info!("This is optional info and printed to stdout");
+    debug!("This is optional debug and printed to stdout");
+    trace!("This is optional trace and printed to stdout");
 }
+
